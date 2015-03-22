20b2bb927dc764a4bafdc9a85ad7102863a026b6
Jacob Sturm <sturman@mail.uc.edu>
2015-03-22 02:58:38 -0400

    - app now gets the phone's lat and lon coordinates - currently just displays them in sensor1/sensor2 location

--- app/src/main/java/group15/mrthermostat/HomePage.java
+++ app/src/main/java/group15/mrthermostat/HomePage.java
@@ -2,8 +2,13 @@ package group15.mrthermostat;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.text.InputType;
 import android.view.Menu;
@@ -11,27 +16,94 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.DateFormat;
 import java.util.Date;
 
 
-public class HomePage extends Activity {
+public class HomePage extends Activity implements LocationListener {
+
+    private TextView latituteField;
+    private TextView longitudeField;
+    private LocationManager locationManager;
+    private String provider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
 
+        //Set date and time
         String currentDateAndTime = DateFormat.getDateTimeInstance().format(new Date());
         TextView dateText = (TextView) findViewById(R.id.homePage_Date);
         dateText.setText(currentDateAndTime);
 
+        //connect the homepage to the weather fragment
         if (savedInstanceState == null) {
             getFragmentManager().beginTransaction()
                     .add(R.id.weatherbox, new weather())
                     .commit();
         }
+
+        latituteField = (TextView) findViewById(R.id.sensor1);
+        longitudeField = (TextView) findViewById(R.id.sensor2);
+
+        //get the location manager
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        // Define the criteria how to select the location provider -> use default
+        Criteria criteria = new Criteria();
+        provider = locationManager.getBestProvider(criteria, false);
+        Location location = locationManager.getLastKnownLocation(provider);
+
+        // Initialize the location fields
+        if (location != null) {
+            System.out.println("Provider " + provider + " has been selected.");
+            onLocationChanged(location);
+        } else {
+            latituteField.setText("Location not available");
+            longitudeField.setText("Location not available");
+        }
+    }
+
+    /* Request updates at startup */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        locationManager.requestLocationUpdates(provider, 400, 1, this);
+    }
+
+    /* Remove the locationlistener updates when Activity is paused */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        locationManager.removeUpdates(this);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        int lat = (int) (location.getLatitude());
+        int lng = (int) (location.getLongitude());
+        latituteField.setText(String.valueOf(lat));
+        longitudeField.setText(String.valueOf(lng));
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Toast.makeText(this, "Enabled new provider " + provider,
+                Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Toast.makeText(this, "Disabled provider " + provider,
+                Toast.LENGTH_SHORT).show();
     }
 
     public void openProfilesActivity(View view) {
